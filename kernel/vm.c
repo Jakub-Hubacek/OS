859add3dc4740ec7cf87bfad536470952c6c43c1
Jakub Hubacek <xhubacek@stuba.sk>
2023-11-06 22:24:05 +0100

    vmprint done

--- kernel/vm.c
+++ kernel/vm.c
@@ -449,3 +449,29 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void vmprint(pagetable_t pagetable){
+  static int level = 0;
+  if (level == 0)
+  {
+    printf("page table %p\n", pagetable);
+  }
+      // there are 2^9 = 512 PTEs in a page table.
+  for (int i = 0; i < 512; i++)
+  {
+    
+    pte_t pte = pagetable[i];
+    if (pte & PTE_V){
+      for (int j = 0; j <=level; j++)
+        printf(" ..");
+      
+      printf("%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+
+      if ((pte & (PTE_R | PTE_W | PTE_X)) == 0){
+        level++;
+        vmprint((pagetable_t)(PTE2PA(pte)));
+        level--;
+      }
+    }
+  }
+}
